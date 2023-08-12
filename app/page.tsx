ed565d1786a156c41dca4b1d9356fe819ce4f05f
Saksham <55906931+sakshamAK@users.noreply.github.com>
2023-08-12 08:57:00 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,51 @@
-import Image from 'next/image'
+import style from "./page.module.css"
+import { BsBrightnessHigh } from "react-icons/bs";
+import { FaTemperatureFull } from "react-icons/fa6";
+import { PiCircleHalfFill } from "react-icons/pi";
+import { ImBrightnessContrast } from "react-icons/im";
+import { BiCrop } from "react-icons/bi";
+import { MdOutlineRotate90DegreesCcw } from "react-icons/md";
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={`grid grid-cols-8 grid-rows-6 h-screen place-items-center ${style.gridarea}`}>
+      <div className={`${style.preview} flex flex-col gap-4 items-center p-5 h-full w-4/5`}>
+        <div className="p-5 rounded-2xl bg-white">
+          <img src="https://picsum.photos/800/800" className="w-full shadow-md rounded-xl" />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={`${style.settings} flex flex-col justify-start gap-12 h-full w-full p-5`}>
+        <div className="flex items-center gap-4 h-max w-full">
+          <h1 className="text-4xl font-bold">Edit Your Photo</h1>
+          <div className="text-2xl ml-auto"><ImBrightnessContrast /></div>
+        </div>
+        <div className="flex flex-col gap-4 items-center">
+          <div className="w-full">
+            <h1 className="ml-5">Warmness</h1>
+            <input type="range" className={`${style["thumb-custom"]} ${style["thumb-temp"]} w-full h-12 bg-temp rounded-full appearance-none`} />
+          </div>
+          <div className="w-full">
+            <h1 className="ml-5">Brightness</h1>
+            <input type="range" className={`${style["thumb-custom"]} ${style["thumb-brightness"]} w-full h-12 bg-brightness rounded-full appearance-none`} />
+          </div>
+          <div className="w-full">
+            <h1 className="ml-5">Contrast</h1>
+            <input type="range" className={`${style["thumb-custom"]} ${style["thumb-contrast"]} w-full h-12 bg-bandw rounded-full appearance-none`} />
+          </div>
+        </div>
+        <div className="w-full flex items-center justify-around">
+          <div className="h-20 w-20 radius-lg bg-white flex flex-col items-center justify-center text-2xl rounded-lg cursor-pointer">
+            <BiCrop />
+            <h4 className="text-sm">Crop</h4>
+          </div>
+          <div className="h-20 w-20 radius-lg bg-white flex flex-col items-center justify-center text-2xl rounded-lg cursor-pointer">
+            <MdOutlineRotate90DegreesCcw />
+            <h4 className="text-sm">Rotate</h4>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
+
+export default HomePage
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,12 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'temp': "url('/coolness.png')",
+        'bandw': "url('/brightness.png')",
+        'brightness': "url('/bandw.png')",
+      },
+      boxShadow: {
+        'md': '0 4px 6px -1px rgb(0 0 0 / 0.5), 0 2px 4px -2px rgb(0 0 0 / 0.5)',
       },
     },
   },
